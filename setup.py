8d35e68ee45b05f9e53937ac67aed5c7aa9d69d9
Tom <tkuehnem@gmail.com>
2022-05-03 22:39:51 +0200

    add MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
 from setuptools import setup
 
-# read the contents of your README file
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+
 
 setup(name='cbpi4-settime',
       version='0.0.1',
@@ -21,7 +17,5 @@ setup(name='cbpi4-settime',
       packages=['cbpi4-settime'],
 	    install_requires=[
             'cbpi>=4.0.0.33',
-      ],
-      long_description=long_description,
-      long_description_content_type='text/markdown'
+      ]
      )
